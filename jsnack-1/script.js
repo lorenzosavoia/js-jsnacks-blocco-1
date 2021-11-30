c304e05e4ddb63871dffb5441df2a1cce5aa87b3
lorenzosavoia <lorenzo.savoia97@gmail.com>
2021-12-01 00:02:20 +0100

    init

---
+++ jsnack-1/script.js
@@ -0,0 +1,7 @@
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.Il programma stampa la somma di tutti i numeri inseriti.
+
+
+for (let index = 0; index < 10; index++) {
+    const 
+    
+}
