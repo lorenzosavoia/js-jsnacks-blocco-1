986a0ae6e3d4adb1bf428261c2d9dddfd231ea38
lorenzosavoia <lorenzo.savoia97@gmail.com>
2021-12-01 00:41:15 +0100

    primo snack

--- jsnack-1/script.js
+++ jsnack-1/script.js
@@ -1,7 +1,17 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.Il programma stampa la somma di tutti i numeri inseriti.
+const risultato = document.querySelector(".risultato");
 
+let arrayNumber = [];
+let sum = 0;
 
-for (let index = 0; index < 10; index++) {
-    const 
+for (let i = 0; i < 10; i++) {
     
+    let number = parseInt(prompt("inserisci un numero"));
+    // console.log(number);
+    arrayNumber.push(number);
+    sum += arrayNumber[i];
 }
+// console.log(arrayNumber);
+// console.log(sum);
+risultato.innerHTML = `il risultato è di ${sum}`;
+
